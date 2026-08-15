02b5f26239390e22d617271e26670b268754399e
mgjk04 <159707804+mgjk88@users.noreply.github.com>
2026-08-16 01:59:33 +0800

    feat: Add add & list

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Echo {
     private static final String NAME = "Echo";
     private static final String SEPARATOR = "============================================================";
+    private static final String[] itemArray = new String[100];
+    private static int lastIdx = 0;
 
 
     private static void greet() {
@@ -31,6 +33,24 @@ public class Echo {
         System.out.println(input);
         System.out.println(SEPARATOR);
     }
+
+    private static void add(String item) {
+        if (lastIdx == itemArray.length) {
+            Echo.echo("I can't remember any more! Sorry!");
+            return;
+        }
+        itemArray[lastIdx++] = item;
+        Echo.echo("added: " + item);
+    }
+
+    private static void list() {
+        System.out.println(SEPARATOR);
+        for (int i = 1; i <= lastIdx; ++i) {
+            String entry = i + ". " + itemArray[i - 1];
+            System.out.println(entry);
+        }
+        System.out.println(SEPARATOR);
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Echo.greet();
@@ -39,7 +59,11 @@ public class Echo {
             if (input.equals("bye")) {
                 break;
             }
-            Echo.echo(input);
+            if (input.equals("list")) {
+                Echo.list();
+                continue;
+            }
+            Echo.add(input);
         }
         Echo.farewell();
         scanner.close();
