9df0212465ba2c4eb5f00861b0a5cec6b75300c0
mgjk04 <159707804+mgjk88@users.noreply.github.com>
2026-08-15 22:34:17 +0800

    chore: Rename chatbot
    Replace all instances of Duke with Echo as per level 0 part a
    requirements, inclusive of banner.

---
+++ src/main/java/Echo.java
@@ -0,0 +1,10 @@
+public class Echo {
+    public static void main(String[] args) {
+        String banner = " _____     _           \n"
+                      + "| ____|___| |__   ___  \n"
+                      + "|  _| / __| '_ \\ / _ \\ \n"
+                      + "| |__| (__| | | | (_) |\n"
+                      + "|_____\\___|_| |_|\\___/ \n";
+        System.out.println(banner);
+    }
+}
