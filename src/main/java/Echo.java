f6dbca938935f1f48db78b83bc430572000248a0
mgjk04 <159707804+mgjk88@users.noreply.github.com>
2026-08-15 23:28:45 +0800

    feat: Add greet and farewell functions
    Chatbot previously only prints banner. Now includes ability to greet and
    bid farewell to users. Includes separators.

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,10 +1,27 @@
 public class Echo {
-    public static void main(String[] args) {
+    private static final String name = "Echo";
+    private static final String separator = "============================================================";
+    private static void greet() {
         String banner = " _____     _           \n"
                       + "| ____|___| |__   ___  \n"
                       + "|  _| / __| '_ \\ / _ \\ \n"
                       + "| |__| (__| | | | (_) |\n"
                       + "|_____\\___|_| |_|\\___/ \n";
+        String salutation = "Hello! I'm " + name + ".\n" +
+                          "How can I help?";
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println(salutation);
+        System.out.println(separator);
+    }
+
+    private static void farewell() {
+        String valediction = "Bye!";
+        System.out.println(valediction);
+        System.out.println(separator);
+    }
+    public static void main(String[] args) {
+        Echo.greet();
+        Echo.farewell();
     }
 }
