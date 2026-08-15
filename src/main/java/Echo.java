89211213ba875506154eed2753fdbd39df4d8e21
mgjk04 <159707804+mgjk88@users.noreply.github.com>
2026-08-16 00:21:30 +0800

    feat: Add echo
    Echo repeats the user's commands until the command `bye` is issued.

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,27 +1,47 @@
+import java.util.Scanner;
+
 public class Echo {
-    private static final String name = "Echo";
-    private static final String separator = "============================================================";
+    private static final String NAME = "Echo";
+    private static final String SEPARATOR = "============================================================";
+
+
     private static void greet() {
         String banner = " _____     _           \n"
                       + "| ____|___| |__   ___  \n"
                       + "|  _| / __| '_ \\ / _ \\ \n"
                       + "| |__| (__| | | | (_) |\n"
                       + "|_____\\___|_| |_|\\___/ \n";
-        String salutation = "Hello! I'm " + name + ".\n" +
+        String salutation = "Hello! I'm " + NAME + ".\n" +
                           "How can I help?";
-        System.out.println(separator);
+        System.out.println(SEPARATOR);
         System.out.println(banner);
         System.out.println(salutation);
-        System.out.println(separator);
+        System.out.println(SEPARATOR);
     }
 
     private static void farewell() {
         String valediction = "Bye!";
+        System.out.println(SEPARATOR);
         System.out.println(valediction);
-        System.out.println(separator);
+        System.out.println(SEPARATOR);
+    }
+
+    private static void echo(String input) {
+        System.out.println(SEPARATOR);
+        System.out.println(input);
+        System.out.println(SEPARATOR);
     }
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Echo.greet();
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            Echo.echo(input);
+        }
         Echo.farewell();
+        scanner.close();
     }
 }
