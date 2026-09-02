4f6b7179ec93ef94b73346c6eca9fdcbc8ac9a44
mgjk04 <159707804+mgjk88@users.noreply.github.com>
2026-09-03 06:14:20 +0800

    fix: Add title to stage

--- src/main/java/echo/Main.java
+++ src/main/java/echo/Main.java
@@ -30,6 +30,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Echo");
             stage.setMinWidth(417);
             stage.setMinHeight(220);
             fxmlLoader.<MainWindow>getController().setEcho(echo);
